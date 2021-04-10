58c42e52e1cc514e61ea2d63c1c3fd094e3b1696
Nkululeko <ngcobonw@gmail.com>
2021-04-10 13:39:40 +0200

    Dice flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,57 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
-    MaterialApp(
+    MyDice(),
+  );
+}
+
+class MyDice extends StatefulWidget {
+  @override
+  _MyDiceState createState() => _MyDiceState();
+}
+
+class _MyDiceState extends State<MyDice> {
+  int leftDice = 1;
+  int rightDice = 1;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: Center(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                  onPressed: () async {
+                    setState(() {
+                      leftDice = Random().nextInt(6) + 1;
+                    });
+                  },
+                  child: Image.asset('images/dice$leftDice.png'),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () async {
+                    setState(() {
+                      rightDice = Random().nextInt(6) + 1;
+                    });
+                  },
+                  child: Image.asset('images/dice$rightDice.png'),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
-}
-
-class DicePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+    );
   }
 }
